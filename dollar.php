522ee6f3627dc5fb9bb1886d1d13cf7833021ea0
Paulo Henrique Martins <paulovelho@paulovelho.com>
2015-08-20 02:42:06 +0200

    fixing path to be easier to change

--- dollar.php
+++ dollar.php
@@ -8,25 +8,28 @@
 	$cent50 = "R$ ".number_format($rate/2, 2, ',', '.');
 	$rate = "R$ ".number_format($rate, 2, ',', '.');
 
+
 	$images = array("50cent_1.jpg", "50cent_2.jpg", "50cent_3.jpg", "50cent_4.jpg", "50cent-newyork.jpg");
-	$imagePath = "/Users/paulovelho/Sites/paulovelho/dollar/images/";
+	$dollarPath = "/Users/paulovelho/Sites/paulovelho/dollar";
+	$imagePath = $dollarPath."/images/";
 	
 	$cent = $images[array_rand($images)];
 
 	$fontBorder = 3;
 	$fontMarginBottom = 20;
+	$fontPath = $dollarPath."/lib/Impact.ttf";
 
 	$image = WideImage::load($imagePath.$cent)->resize(500);
 	$canvas = $image->getCanvas();
-	$canvas->useFont('/Users/paulovelho/Sites/paulovelho/dollar/lib/Impact.ttf', 38, $image->allocateColor(0, 0, 0));
+	$canvas->useFont($fontPath, 38, $image->allocateColor(0, 0, 0));
 	$canvas->writeText('center+'.$fontBorder, 'bottom-'.($fontMarginBottom-$fontBorder), $cent50);
-	$canvas->useFont('/Users/paulovelho/Sites/paulovelho/dollar/lib/Impact.ttf', 38, $image->allocateColor(0, 0, 0));
+	$canvas->useFont($fontPath, 38, $image->allocateColor(0, 0, 0));
 	$canvas->writeText('center+'.$fontBorder, 'bottom-'.($fontMarginBottom+$fontBorder), $cent50);
-	$canvas->useFont('/Users/paulovelho/Sites/paulovelho/dollar/lib/Impact.ttf', 38, $image->allocateColor(0, 0, 0));
+	$canvas->useFont($fontPath, 38, $image->allocateColor(0, 0, 0));
 	$canvas->writeText('center-'.$fontBorder, 'bottom-'.($fontMarginBottom-$fontBorder), $cent50);
-	$canvas->useFont('/Users/paulovelho/Sites/paulovelho/dollar/lib/Impact.ttf', 38, $image->allocateColor(0, 0, 0));
+	$canvas->useFont($fontPath, 38, $image->allocateColor(0, 0, 0));
 	$canvas->writeText('center-'.$fontBorder, 'bottom-'.($fontMarginBottom+$fontBorder), $cent50);
-	$canvas->useFont('/Users/paulovelho/Sites/paulovelho/dollar/lib/Impact.ttf', 38, $image->allocateColor(255, 255, 255));
+	$canvas->useFont($fontPath, 38, $image->allocateColor(255, 255, 255));
 	$canvas->writeText('center', 'bottom-20', $cent50);
 
 
